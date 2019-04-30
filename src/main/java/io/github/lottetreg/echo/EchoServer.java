20af7a158dbff12cd21c93dc111b94a92e5e3278
Charlotte Tregelles <charlotte@8thlight.com>
2019-04-30 18:07:43 -0400

    add Connection to wrap Socket, return new Connection in Socket.accept()

--- src/main/java/io/github/lottetreg/echo/EchoServer.java
+++ src/main/java/io/github/lottetreg/echo/EchoServer.java
@@ -1,17 +1,12 @@
 package io.github.lottetreg.echo;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-
 public class EchoServer {
   public static void main(String[] args) {
     int portNumber = new ArgumentParser().parse(args);
-    try {
-      ServerSocket serverSocket = new ServerSocket(portNumber);
-      System.out.println("Waiting for connection");
-      serverSocket.accept();
-      System.out.println("Connection accepted");
-    } catch(IOException e) {
-    }
+    Socket socket = new Socket();
+    socket.setPort(portNumber);
+
+    System.out.println("Waiting for connection");
+    socket.acceptConnection();
   }
 }
--- src/test/java/io/github/lottetreg/echo/SocketTest.java
+++ src/test/java/io/github/lottetreg/echo/SocketTest.java
@@ -3,63 +3,73 @@ package io.github.lottetreg.echo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class SocketTest {
+  private boolean calledAccept;
+  private Socket socket;
 
   private class MockServerSocket extends ServerSocket {
-
-    public boolean called = false;
-
-    public MockServerSocket() throws IOException {
-    }
+    public MockServerSocket() throws IOException {}
 
     public java.net.Socket accept() {
-      this.called = true;
+      SocketTest.this.calledAccept = true;
       return new java.net.Socket();
     }
   }
 
+  @Before
+  public void setUp() {
+    socket = new Socket();
+  }
+
+  @After
+  public void tearDown() {
+    socket.close();
+  }
+
   @Test
   public void testCreatesAServerSocket() {
-    Socket socket = new Socket();
     assertThat(socket.serverSocket, instanceOf(ServerSocket.class));
   }
 
   @Test
   public void testSetServerSocket() throws IOException {
     ServerSocket serverSocket = new ServerSocket();
-    Socket socket = new Socket();
 
     socket.setServerSocket(serverSocket);
 
-    assertEquals(socket.serverSocket, serverSocket);
+    assertEquals(serverSocket, socket.serverSocket);
   }
 
   @Test
-  public void testSetPort() throws IOException {
-    Socket socket = new Socket();
-
+  public void testSetPort() {
     socket.setPort(9000);
 
-    assertEquals(socket.serverSocket.getLocalPort(), 9000);
+    assertEquals(9000, socket.serverSocket.getLocalPort());
   }
 
   @Test
   public void testAcceptConnection() throws IOException {
     ServerSocket mockServerSocket = new MockServerSocket();
-    Socket socket = new Socket();
     socket.setServerSocket(mockServerSocket);
 
     socket.setPort(9000);
     socket.acceptConnection();
 
-    MockServerSocket mockSocket = (MockServerSocket) socket.serverSocket;
-    assertEquals(mockSocket.called, true);
+    assertEquals(true, calledAccept);
   }
 
+  @Test
+  public void testClose() {
+    socket.close();
 
+    assertEquals(true, socket.serverSocket.isClosed());
+  }
 }
