7e6e877430d2bf153c8f803d392a130172bde185
Charlotte Tregelles <charlotte@8thlight.com>
2019-04-30 11:46:19 -0400

    test socket connection with thread

--- src/test/java/io/github/lottetreg/echo/EchoServerTest.java
+++ src/test/java/io/github/lottetreg/echo/EchoServerTest.java
@@ -3,25 +3,22 @@ package io.github.lottetreg.echo;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServerTest {
-  @Test
-  public void printsErrorMessageWhenConnectionFails() {
-//    Connection connection = new MockConnection(serverSocket);
-//    EchoServer.main(connection);
+  int portNumber = 8080;
 
-//    Assert.assertEquals(8080, echoServer.serverSocket.getLocalPort());
+  @Test
+  public void itWorks() throws IOException {
+    Thread serverHandlerThread = new ServerHandlerThread();
+    serverHandlerThread.start();
+    new Socket("0.0.0.0", portNumber);
   }
 
-  private class MockConnection extends Connection {
-    MockConnection(ServerSocket socket) {
-      super(socket);
-    }
-
-    public Socket accept() throws IOException {
-      throw new IOException();
+  class ServerHandlerThread extends Thread {
+    public void run() {
+      String[] args = {Integer.toString(portNumber)};
+      EchoServer.main(args);
     }
   }
 }
